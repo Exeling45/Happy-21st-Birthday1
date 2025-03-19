23ea1d6c95aa481c3d977ebd9b45271f82c27bd7
Exeling45 <mdudiiskandar2@gmail.com>
2025-03-19 22:22:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+alert('hellow broo met ultah yah maaf telat awokwok')
+
+alert('Semoga dengan bertambahnya umur lebih dewasa')
+
+alert('Maaf bro ngucapinnya lewat link sekalian gw ngehasilin project lewat ultah lau')
+
+alert('awokwok hbd ya cees')
